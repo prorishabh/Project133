28d04e3a6288d0b5830b31f6ba64985c5592becc
prorishabh <93649567+prorishabh@users.noreply.github.com>
2024-03-02 21:06:00 +0530

    Add files via upload

---
+++ TeddyBearmain.js
@@ -0,0 +1,56 @@
+teddybear_img="";
+status="";
+objects=[];
+
+function preload()
+{
+    teddybear_img=loadImage('Teddy Bear.jpg');
+}
+
+function setup()
+{
+    canvas=createCanvas(640,350);
+    canvas.position(600,200);
+    object_detector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status:Detecting Object";
+}
+
+function modelLoaded()
+{
+    console.log("Model is Loaded");
+    status=true;
+    object_detector.detect(teddybear_img,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        objects=results;
+    }
+}
+
+function draw()
+{
+    image(teddybear_img,0,0,640,350);
+
+    if(status != "")
+    {
+        for(i=0;i<objects.length;i++)
+        {
+           document.getElementById("status").innerHTML="Status:Object Detected"; 
+
+           fill('#fc0303');
+           noFill();
+           stroke('#fc0303');
+           percent=floor(objects[i].confidence*100);
+           text(objects[i].label+" "+percent+" %",objects[i].x,objects[i].y);
+           rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
